d919fc8cb38428d3b10c6822ca6f6ba2ad40b1cc
RedRubio <144962731+RedRubio@users.noreply.github.com>
2024-09-16 20:25:54 -0700

    Combined Eric and Chris Work
    Combined Eric and Chris' scenes into one branch

---
+++ MainTest/Menu.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	// Called when the Play button is pressed
+	private void Play()
+	{
+		GetTree().ChangeSceneToFile("res://world.tscn");
+	}
+
+	// Called when the Options button is pressed
+	private void Options()
+	{
+		GetTree().ChangeSceneToFile("res://options_menu.tscn");
+	}
+
+	// Called when the Quit button is pressed
+	private void Quit()
+	{
+		GetTree().Quit();
+	}
+}
