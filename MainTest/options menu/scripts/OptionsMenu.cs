18ac3832943930937b4923b003cd6bc23498d93d
CAvila274 <christianavila.1492@gmail.com>
2024-09-18 19:33:30 -0700

    windows mode and keybinds

---
+++ MainTest/options menu/scripts/OptionsMenu.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class OptionsMenu : Control
+{
+	  // Called when the Back button is pressed
+	private void Back()
+	{
+		GetTree().ChangeSceneToFile("res://main menu/menu.tscn");
+	}
+}
