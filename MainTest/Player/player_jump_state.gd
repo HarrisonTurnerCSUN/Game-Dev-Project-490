8424536d81e3cc89373346897148ddc3cca1ca25
Jonathan Johnson <jon01mtnz@hotmail.com>
2024-09-25 17:28:31 -0700

    Player State machine inprog
    Started making player use state machine based on tutorial. moved Statemachine code into utilities folder. Testing on playerStateTest.

---
+++ MainTest/Player/player_jump_state.gd
@@ -0,0 +1,20 @@
+extends NodeState
+
+@export var character_body_2d : CharacterBody2D
+@export var animated_sprite_2d : AnimatedSprite2D
+
+@export_category("Jump state")
+
+
+func on_process(_delta :float):
+	pass
+	
+func on_physics_process(_delta :float):
+	pass
+	
+func enter():
+	pass
+	
+func exit():
+	pass
+	
