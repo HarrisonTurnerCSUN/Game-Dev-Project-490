159f0df75c11a2a2526db761db05a9948a7427e5
RedRubio <144962731+RedRubio@users.noreply.github.com>
2024-09-29 19:10:40 -0700

    bat and Menu sound
    fixed the bat with some new tracking tech that requires tracking regions and added some audio stuff to the menu that can be fined tuned later

--- MainTest/Enemies/Bat/aggro.gd
+++ MainTest/Enemies/Bat/aggro.gd
@@ -2,51 +2,43 @@ extends NodeStateBat
 
 @export var character_body_2d : CharacterBody2D
 @export var animated_sprite_2d : AnimatedSprite2D
-@export var speed : int
+@export var speed : int = 100  # Ensure a reasonable default speed
 @onready var bat_aggro: AudioStreamPlayer2D = $"../../batAggro"
+@onready var navigation_agent_2d: NavigationAgent2D = $"../../NavigationAgent2D"
 
 var player : CharacterBody2D
 var max_speed : int
 var first_time = true
 var is_playing = false
 
-func on_process(_delta :float):
-	pass
-	
-func on_physics_process(delta :float):
-	var direction : int
-	var ydirection: int
-	
-	if character_body_2d.global_position.y < player.global_position.y:
-		ydirection = -1
-	elif character_body_2d.global_position.y > player.global_position.y:
-		ydirection = 1
-	
-	if character_body_2d.global_position > player.global_position:
-		animated_sprite_2d.flip_h = true
-		direction = -1
-	elif character_body_2d.global_position < player.global_position:
-		animated_sprite_2d.flip_h = false
-		direction = 1
+func on_physics_process(delta : float):
+	if navigation_agent_2d.is_navigation_finished() and \
+			player.global_position.distance_to(navigation_agent_2d.target_position) < 1.0:
+		return
+
+	navigation_agent_2d.target_position = player.global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+
+
+	character_body_2d.velocity = character_body_2d.global_position.direction_to(next_path_position) * speed
+
+	character_body_2d.move_and_slide()
 	if first_time:
 		bat_aggro.play()
 		first_time = false
 	if not is_playing:
 		bat_aggro.play()
 		is_playing = true
-	character_body_2d.velocity.x += direction * speed * delta
-	character_body_2d.velocity.y += direction * speed * delta
-	character_body_2d.velocity.x = clamp(character_body_2d.velocity.x, -max_speed, max_speed)
-	character_body_2d.velocity.y = clamp(character_body_2d.velocity.y, -max_speed, max_speed)
-	character_body_2d.move_and_slide()
-	
+		
 func enter():
-	player = get_tree().get_nodes_in_group("Player")[0] as CharacterBody2D
-	max_speed = speed + 20
-	
+	if first_time:
+		player = get_tree().get_nodes_in_group("Player")[0] as CharacterBody2D
+		max_speed = speed + 20
+		first_time = false
+		print("Player assigned: ", player)
+
 func exit():
 	pass
-
 
 func _on_bat_aggro_finished() -> void:
 	is_playing = false
