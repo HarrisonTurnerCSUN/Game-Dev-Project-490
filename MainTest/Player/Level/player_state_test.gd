e945fc36ffb74da9849907dabbb1c902a12b8cca
RedRubio <144962731+RedRubio@users.noreply.github.com>
2024-10-13 16:00:11 -0700

    new trees and tasks
    updated soem nemy trees and dasks

--- MainTest/Player/Level/player_state_test.gd
+++ MainTest/Player/Level/player_state_test.gd
@@ -1,6 +1,36 @@
 extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+var _moved_this_frame: bool = false
+var _is_dead: bool = false
 
+## When agent is damaged...
+func _damaged(_amount: float, knockback: Vector2) -> void:
+	apply_knockback(knockback)
+	#animation_player.play(&"hurt")
+	var btplayer := get_node_or_null(^"BTPlayer") as BTPlayer
+	if btplayer:
+		btplayer.set_active(false)
+	var hsm := get_node_or_null(^"LimboHSM")
+	if hsm:
+		hsm.set_active(false)
+	#await animation_player.animation_finished
+	if btplayer and not _is_dead:
+		btplayer.restart()
+	if hsm and not _is_dead:
+		hsm.set_active(true)
+		
 #func receives_knockback(damage_source_pos: Vector2, received_damage: int):
 	#pass
+func apply_knockback(knockback: Vector2, frames: int = 10) -> void:
+	if knockback.is_zero_approx():
+		return
+	for i in range(frames):
+		move(knockback)
+		await get_tree().physics_frame
+
+func move(p_velocity: Vector2) -> void:
+	velocity = lerp(velocity, p_velocity, 0.2)
+	move_and_slide()
+	_moved_this_frame = true
+	
