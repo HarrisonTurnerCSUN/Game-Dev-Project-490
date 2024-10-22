7a2e49b695ace38ee71407aa122a800d00e07632
HarrisonTurnerCSUN <harrison.turner.887@my.csun.edu>
2024-10-21 19:25:30 -0700

    Revert "Merge branch 'Jonathan' into Harry"
    This reverts commit f83683667dcf4fd5841c6878cc23d58c6df45cb1, reversing
    changes made to 1758f6f73034a15841aa9aec804a111d0e83a31d.

--- MainTest/Overworld/playerOverworld/idle_overworld.gd
+++ MainTest/Overworld/playerOverworld/idle_overworld.gd
@@ -2,12 +2,9 @@ extends NodeState
 
 @export var character_body_2d : CharacterBody2D
 @export var animation_player: AnimatedSprite2D
-@export var Animation_Tree: AnimationTree
 @export_category("Friction")
 @export var friction : int = 500
-var direction: Vector2
-#var last_direction := Vector2(0,-1)
-var last_direction : Vector2
+
 func on_process(_delta :float):
 	pass
 	
@@ -16,17 +13,12 @@ func on_physics_process(_delta :float):
 	character_body_2d.velocity.x = move_toward(character_body_2d.velocity.x,0,friction)
 	character_body_2d.velocity.y = move_toward(character_body_2d.velocity.y,0,friction)
 	
+	character_body_2d.move_and_slide()
+	
+	#Transitions
 	var direction_x : float = GameInputEvents.movement_input()
 	var direction_y : float = GameInputEvents.movement_input_y()
 	
-	direction.x = GameInputEvents.movement_input()
-	direction.y = GameInputEvents.movement_input_y()
-	var input = direction.normalized()
-	#last_direction = character_body_2d.velocity.normalized()
-	#Animation_Tree.set("parameters/Idle_BlendSpace2D/blend_position",input)
-	
-	character_body_2d.move_and_slide()
-	#Transitions
 	if direction_x or direction_y :
 		transition.emit("Walk")
 		
@@ -34,16 +26,7 @@ func on_physics_process(_delta :float):
 		transition.emit("Run")
 	
 func enter():
-	direction.x = GameInputEvents.movement_input()
-	direction.y = GameInputEvents.movement_input_y()
-	
-	var input = direction.normalized()
-	#last_direction = character_body_2d.velocity.normalized()
-	#Animation_Tree.set("parameters/Idle_BlendSpace2D/blend_position",input)
-	
-	character_body_2d.move_and_slide()
-	
+	animation_player.play("Idle")
 	
 func exit():
-	#animation_player.stop()
-	pass
+	animation_player.stop()
