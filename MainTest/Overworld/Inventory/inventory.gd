22637488dd588f3541dfa335de6e8a79f6d28ec9
HarrisonTurnerCSUN <harrison.turner.887@my.csun.edu>
2024-11-04 16:43:15 -0800

    inventory
    added inventory functionality

--- MainTest/Overworld/Inventory/inventory.gd
+++ MainTest/Overworld/Inventory/inventory.gd
@@ -4,11 +4,30 @@ class_name Inventory
 
 signal updated
 
-@export var items: Array[InventoryItem]
+@export var slots: Array[InventorySlot]
+
 
 func insert(item: InventoryItem):
-	for i in range(items.size()):
-		if !items[i]:
-			items[i] = item
-			break
-	updated.emit()
+	var itemSlots = slots.filter(func(slot): return slot.item == item and slot.amount < item.maxAmountPrStack)
+	
+	if !itemSlots.is_empty():
+		itemSlots[0].amount += 1
+		updated.emit()
+		return
+	
+	var emptySlots = slots.filter(func(slot): return slot.item == null)
+	if !emptySlots.is_empty():
+		emptySlots[0].item = item
+		emptySlots[0].amount = 1
+		updated.emit()
+
+
+func removeSlot(inventorySlot: InventorySlot):
+	var index = slots.find(inventorySlot)
+	if index < 0: return
+	
+	slots[index] = InventorySlot.new()
+	
+
+func insertSlot(index: int, inventorySlot: InventorySlot):
+	slots[index] = inventorySlot
