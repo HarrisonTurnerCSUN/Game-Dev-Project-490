3bf3ef9c4c1b03b173312f3695b37c9a107e57f2
Jonathan Johnson <jon01mtnz@hotmail.com>
2024-11-11 14:59:43 -0800

    Pause Menu

---
+++ MainTest/main menu/Pause Menu/in_game_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+func resume():
+	get_tree().paused = false
+	$".".hide()
+	
+func pause():
+	get_tree().paused = true
+	$".".show()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Escape") and get_tree().paused == true:
+		resume()
+	elif Input.is_action_just_pressed("Escape") and get_tree().paused == false:
+		pause()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main menu/menu.tscn")
