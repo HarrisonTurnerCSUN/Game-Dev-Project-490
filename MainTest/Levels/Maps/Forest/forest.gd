364c1edd960a08d333ef487e19caf1bffe719236
Jonathan Johnson <jon01mtnz@hotmail.com>
2024-11-27 19:09:37 -0800

    Pause menu + forest changes etc

--- MainTest/Levels/Maps/Forest/forest.gd
+++ MainTest/Levels/Maps/Forest/forest.gd
@@ -13,7 +13,11 @@ func _process(delta: float) -> void:
 	var player_tile_data = tilemaplayer.get_cell_tile_data(tilemaplayer.local_to_map(player.position))
 	if player_tile_data:
 		if player_tile_data.get_custom_data("Fluid"): 
-			player.velocity -= delta*100*player.velocity
+			player.velocity.x = clamp(player.velocity.x, -100, 100)
+			player.velocity.y = clamp(player.velocity.y, -400, 100)
+		if player_tile_data.get_custom_data("Slow"):
+			print("yes")
+			player.velocity.x = clamp(player.velocity.x, -100, 100)
 	pass
 
 
