91fec42a30c2c5a13a3deb3d0b2e1a9c00098fb4
HarrisonTurnerCSUN <harrison.turner.887@my.csun.edu>
2025-04-07 18:47:32 -0700

    map + collision + coins

--- MainTest/Overworld/Inventory/inventoryItem.gd
+++ MainTest/Overworld/Inventory/inventoryItem.gd
@@ -5,3 +5,18 @@ class_name InventoryItem
 @export var name: String = ""
 @export var texture: Texture2D
 @export var maxAmountPrStack: int = 10
+
+func use(user) -> bool:
+	if name == "Coin":
+		var inventory = user.get_inventory()
+		var amount_to_remove = 10
+
+		if inventory.count(self) >= amount_to_remove:
+			inventory.remove(self, amount_to_remove)
+			print("Used 10 coins!")
+			return true
+		else:
+			print("Not enough coins.")
+			return false
+
+	return false
